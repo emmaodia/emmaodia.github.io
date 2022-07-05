a38ad1013bb0303ccb4f01bdaa3bb5b56d5f2b94
emmaodia <odia.emma@gmail.com>
2022-07-05 18:54:41 +0100

    switched to gatsby

---
+++ src/pages/portfolio-links.js
@@ -0,0 +1,59 @@
+export const links = [
+  {
+    text: "Token Faucet",
+    url: "https://www.gatsbyjs.com/docs/tutorial",
+    description:
+      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+  },
+  {
+    text: "Yield Farming",
+    url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
+    description:
+      "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites.",
+  },
+  {
+    text: "Liquidity Staking",
+    url: "https://www.gatsbyjs.com/plugins",
+    description:
+      "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
+  },
+  {
+    text: "Lottery",
+    url: "https://www.gatsbyjs.com/cloud",
+    description:
+      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+  },
+]
+
+export const samplePageLinks = [
+  {
+    text: "blog",
+    url: "blog",
+    badge: false,
+    description: "A collection of my writings",
+  },
+  { text: "github", url: "https://github.com/emmaodia" },
+  { text: "linkedIn", url: "https://linkedin.com/in/emmaodia" },
+  { text: "twitter", url: "https://twitter.com/emmaodia" },
+]
+
+export const moreLinks = [
+  { text: "Join us on Discord", url: "https://gatsby.dev/discord" },
+  {
+    text: "Documentation",
+    url: "https://gatsbyjs.com/docs/",
+  },
+  {
+    text: "Starters",
+    url: "https://gatsbyjs.com/starters/",
+  },
+  {
+    text: "Showcase",
+    url: "https://gatsbyjs.com/showcase/",
+  },
+  {
+    text: "Contributing",
+    url: "https://www.gatsbyjs.com/contributing/",
+  },
+  { text: "Issues", url: "https://github.com/gatsbyjs/gatsby/issues" },
+]
